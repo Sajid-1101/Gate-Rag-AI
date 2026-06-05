b42710440a6d62ae2a0fe4132e05fc4ccf0dbb2f
Sajid <sajidsaleem1101@gmail.com>
2026-06-05 16:31:47 +0530

    removing raw data

--- backend/server.py
+++ backend/server.py
@@ -2,30 +2,22 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 from src.rag_pipeline import get_rag_response
-
-
 app = Flask(__name__)
-
 CORS(app)
 
 @app.route("/", methods=["GET"])
 def home():
-
     return {
         "message": "GATE CSE RAG Assistant API Running"
     }
-
+    
 @app.route("/chat", methods=["POST"])
 def chat():
 
-
     data = request.json
-
-
     question = data.get(
         "question"
     )
-
 
     if not question:
 
@@ -34,22 +26,14 @@ def chat():
                 "error":"Question is required"
             }
         ),400
-
-
-
     answer = get_rag_response(
         question
     )
-
-
     return jsonify(
         {
             "answer": answer
         }
     )
-
-
-
 if __name__ == "__main__":
 
     app.run(
